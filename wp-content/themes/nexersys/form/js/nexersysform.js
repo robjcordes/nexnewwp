be169a5a3963318cd51fb98ee3c4c66b8c539172
Jordan Austin <jordotech@Jordan-Austins-MacBook-Pro.local>
2012-08-24 17:18:55 -0500

    hidden div for hidden lead codes

--- wp-content/themes/nexersys/form/js/nexersysform.js
+++ wp-content/themes/nexersys/form/js/nexersysform.js
@@ -1,15 +1,17 @@
 var $ = jQuery.noConflict();
 $(document).ready(function() {
+    $(".campaign_title").hide();
     $(".nexersys_landing_form").load('/wp-content/themes/nexersys/form/nexersysform.html', function(){
         nexReqForm.checkUrlType();
         nexReqForm.formatForm();
         nexReqForm.submitButtonBehavior();
         nexReqForm.attachValidation();
     });
-
+    var thisURL = $(location).attr('href');
+    var campaignName = $('.campaign_title').html();
     nexReqForm = {
-    hiddenCommInput : "<input type='hidden' name='xnQsjsdp' value='dhOYVrEbdmJthYo*kRl79w$$'><input type='hidden' name='xmIwtLD' value='xxtEl*Hx2ByNU*uyPq3OMHBogz4VdScW'><input type='hidden' name='actionType' value='TGVhZHM='><input type='hidden' name='returnURL' value='http://nexersys.com/thank-you'>",
-    hiddenHomeInput : "<input type='hidden' name='xnQsjsdp' value='f1LzxolSe-0$'><input type='hidden' name='xmIwtLD' value='82AfV3HJwjp71g4ILMIYL*GCpUdN*P5O'><input type='hidden' name='actionType' value='TGVhZHM='><input type='hidden' name='returnURL' value='http://nexersys.com/thank-you'>",
+    hiddenCommInput : "<input type='hidden' name='xnQsjsdp' value='dhOYVrEbdmJthYo*kRl79w$$'><input type='hidden' name='xmIwtLD' value='xxtEl*Hx2ByNU*uyPq3OMHBogz4VdScW'><input type='hidden' name='actionType' value='TGVhZHM='><input type='hidden' name='returnURL' value='http://nexersys.com/thank-you'><input type='hidden' name='LEADCF1' value='"+ campaignName + "'><input type='hidden' name='LEADCF5' value='" + thisURL + "'>",
+    hiddenHomeInput : "<input type='hidden' name='xnQsjsdp' value='f1LzxolSe-0$'><input type='hidden' name='xmIwtLD' value='82AfV3HJwjp71g4ILMIYL*GCpUdN*P5O'><input type='hidden' name='actionType' value='TGVhZHM='><input type='hidden' name='returnURL' value='http://nexersys.com/thank-you'><input type='hidden' name='LEADCF13' value='"+ campaignName + "'><input type='hidden' name='LEADCF14' value='" + thisURL + "'>",
     formNameHome : "WebToLeads452985000001485047",
     formNameComm : "WebToLeads582065000000070015",
     companyNameInput : "<tr class='company_name'><td class='label'><label for='CompanyName'>Company Name*</label></td><td><input class='nexersys validate[required]' id='CompanyName' type='text' maxlength='255' name='LEADCF24' /></td></tr>",
@@ -46,6 +48,7 @@ $(document).ready(function() {
         if(crmType == 'zohoHome'){
             $('#nex_request form').attr('name', this.formNameHome);
             $('#nex_request table').prepend(this.hiddenHomeInput);
+            //$('#nex_request table').prepend(this.hiddenHomeCampaignName);
             $('#nex_request #model_type').attr('name', 'LEADCF15');
 			$('#nex_request #CompanyName').attr('name', 'LEADCF29');
             $('#nex_request #nex_request_country').attr('name', 'LEADCF23');
